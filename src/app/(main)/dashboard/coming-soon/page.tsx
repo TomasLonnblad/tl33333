2cdff337cb08d1a7f133d50c819ac4aa71135d0e
Tomas <tomaslonnblad@gmail.com>
2026-01-01 17:55:02 +0100

    Update page.tsx

--- src/app/(main)/dashboard/coming-soon/page.tsx
+++ src/app/(main)/dashboard/coming-soon/page.tsx
@@ -1,8 +1,10 @@
 export default function Page() {
   return (
     <div className="flex h-full flex-col items-center justify-center space-y-2 text-center">
-      <h1 className="font-semibold text-2xl">Page not found.</h1>
-      <p className="text-muted-foreground">This page is under development and will be available in future updates.</p>
+      <h1 className="font-semibold text-2xl">My ghost like to travel</h1>
+      <p className="text-muted-foreground">
+My ghost like to travel so far in the unknown
+My ghost like to travel so deep into your space</p>
     </div>
   );
 }
